534f531e46f718ee1fce04d24c300b1d4a6d1ff6
Stefan Wagner <stefan@pckl00019.uni-regensburg.de>
2026-03-26 12:12:43 +0100

    Enhance PWA: add asset caching to SW and redundant apple-touch-icon sizes for Safari

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,38 @@
-const CACHE_NAME = 'planer-v2-cache-v1';
+const CACHE_NAME = 'planer-v2-cache-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './assistenz.html',
+  './manifest-oa.json',
+  './manifest-ass.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 
-// Basic service worker to enable PWA recognition on iOS
 self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  // Simple pass-through for now
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
